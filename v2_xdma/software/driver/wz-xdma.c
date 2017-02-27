df3dbc6c53e69a8451e1c6069bfa2f6bf76120db
Wojciech M. Zabolotny <wzab@ise.pw.edu.pl>
2017-02-27 10:09:51 +0100

    Call to engine_cyclic_stop surrounded with locking/unlocking of engine->lock The comments in sources are unclear here. Comments before the implementation of engine_cyclic_stop say:
    /**
     * engine_cyclic_stop() - stop a cyclic transfer running on an SG DMA engine
     *
     * engine->lock must be taken
     */
    
    However in cyclic_transfer_teardown it is called just before taking the engine->lock, so probably the lock is not taken yet...
    
    We will check if that fixes the problem.

--- v2_xdma/software/driver/wz-xdma.c
+++ v2_xdma/software/driver/wz-xdma.c
@@ -283,7 +283,9 @@ static int ioctl_do_wz_stop(struct xdma_engine *engine, unsigned long arg)
     ext = &engine->wz_ext;
     //Stop the transfer (?Should it be done?)
     //xdma_engine_stop(engine);
+    spin_lock(&engine_lock);
     engine_cyclic_stop(engine);
+    spin_unlock(&engine->lock);
     //wait until engine stops
     res = wait_event_interruptible(engine->shutdown_wq, !engine->running);
     if (res) {
