1439e2a3810820a7c4ec1e21f8381d0521871605
Wojciech M. Zabolotny <wzab@ise.pw.edu.pl>
2017-03-04 17:02:04 +0100

    Attempt to use the whole buffer set - 4GB.
    Unfortunately, it agin leads to an error:
    {1}[Hardware Error]: Hardware error from APEI Generic Hardware Error Source: 1
    {1}[Hardware Error]: event severity: fatal
    {1}[Hardware Error]:  Error 0, type: fatal
    {1}[Hardware Error]:   section_type: PCIe error
    {1}[Hardware Error]:   port_type: 4, root port
    {1}[Hardware Error]:   version: 1.16
    {1}[Hardware Error]:   command: 0x4010, status: 0x0547
    {1}[Hardware Error]:   device_id: 0000:05:08.0
    {1}[Hardware Error]:   slot: 0
    {1}[Hardware Error]:   secondary_bus: 0x06
    {1}[Hardware Error]:   vendor_id: 0x10b5, device_id: 0x8749
    {1}[Hardware Error]:   class_code: 040600
    {1}[Hardware rror]:   bridge: secondary_status: 0x0000, control: 0x0003
    Kernel panic - not syncing: Fatal hardware error!
    Pid: 0, comm: swapper Tainted: P    B   W  -- ------------    2.6.32-642.13.1.el6.x86_64 #1
    Call Trace:
     <NMI>  [<ffffffff81548301>] ? panic+0xa7/0x179
     [<ffffffff8132ee71>] ? ghes_notify_nmi+0x241/0x250
     [<ffffffff8154f1a5>] ? notifier_call_chain+0x55/0x80
     [<ffffffff8154f20a>] ? atomic_notifier_call_chain+0x1a/0x20
     [<ffffffff810acc8e>] ? notify_die+0x2e/0x30
     [<ffffffff8154ce51>] ? do_nmi+0x1f1/0x350
     [<ffffffff8154c6e3>] ? nmi+0x83/0x90
     [<ffffffff812fc091>] ? intel_idle+0xd1/0x1b0
     <<EOE>>  [<ffffffff810abb68>] ? hrtimer_start+0x18/0x20
     [<ffffffff81442e14>] ? menu_select+0x174/0x390
     [<ffffffff81441d0a>] ? cpuidle_idle_call+0x7a/0xe0
     [<ffffffff81009fe6>] ? cpu_idle+0xb6/0x110
     [<ffffffff8152f22a>] ? rest_init+0x7a/0x80
     [<ffffffff81c3b127>] ? start_kernel+0x429/0x436
     [<ffffffff81c3a33a>] ? x86_64_start_reservations+0x125/0x129
     [<ffffffff81c3a453>] ? x86_64_start_kernel+0x115/0x124
    Rebooting in 30 seconds..

--- v2_xdma/software/include/wz-xdma-consts.h
+++ v2_xdma/software/include/wz-xdma-consts.h
@@ -3,7 +3,7 @@
 //Size of a single DMA buffer (MUST BE A POWER OF 2!)
 #define WZ_DMA_BUFLEN (4*1024*1024)
 //Number of allocated DMA buffers (MUST BE A POWER OF 2!)
-//Now we reduce the buffer area to 2GB!
-#define WZ_DMA_NOFBUFS 64
+//Now we try to use the whole buffer set - 4GB!
+#define WZ_DMA_NOFBUFS 1024
 //1024
 #endif
