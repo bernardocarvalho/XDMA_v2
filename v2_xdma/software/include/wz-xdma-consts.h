c8c5e1436d93281869f032e712c03483560a338b
Wojciech M. Zabolotny <wzab@ise.pw.edu.pl>
2017-03-05 20:42:50 +0100

    Attempted to use 256 buffers - system crashes with PCIe error.

--- v2_xdma/software/include/wz-xdma-consts.h
+++ v2_xdma/software/include/wz-xdma-consts.h
@@ -4,6 +4,6 @@
 #define WZ_DMA_BUFLEN (4*1024*1024)
 //Number of allocated DMA buffers (MUST BE A POWER OF 2!)
 //Now we try to use the whole buffer set - 4GB!
-#define WZ_DMA_NOFBUFS 128
+#define WZ_DMA_NOFBUFS 256
 //1024
 #endif
